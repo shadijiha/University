95f427ea18665a1e9baf6274d5e10f9299fd442a
jshad0 <jshad0@users.noreply.github.com>
2019-11-01 22:51:39 -0400

    xD

--- Garbage/Aide a kave/Aide a kave/main.c
+++ Garbage/Aide a kave/Aide a kave/main.c
@@ -1,22 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h> // For exit() function
 
-#define _CRT_SECURE_NO_WARNINGS
 
 int main()
 {
-	char c[1000];
-	FILE* fptr;
-	if ((fptr = fopen_s(&fptr, "test.txt", "r")) == NULL)
-	{
-		printf("Error! opening file");
-		// Program exits if file pointer returns NULL.
-		exit(1);
-	}
-	// reads text until newline 
-	fscanf_s(fptr, "%[^\n]", c);
-	printf("Data from the file:\n%s", c);
-	fclose(fptr);
 
 	return 0;
 }
