2902cf7108390f30d398f08fd446d3a2dde0e2b4
Shadi Jiha <shadijiha@gmail.com>
2019-11-10 11:08:54 -0500

    zd

---
+++ COMP-248/COMP248 GUI A4/classes.js
@@ -0,0 +1,236 @@
+/***
+ * 
+ * 
+ */
+
+class Dice  {
+    constructor()  {
+        this.die1 = 0;
+        this.die2 = 0;
+    }
+
+    getDie1()    {   return this.die1;}
+    getDie2()    {   return this.die2;}
+
+    rollDice()  {
+
+        const random = (min, max) =>    {   return Math.round(Math.random() * (max - min) + min)};
+
+        this.die1 = random(1, 6);
+        this.die2 = random(1, 6);
+
+        return this.die1 + this.die2;
+	}
+	
+	sumOfDice()	{
+		return this.die1 + this.die2;
+	}
+
+    isDouble()   {
+        return this.die1 == this.die2;
+    }
+
+    toString()    {
+        return "Die1: " + this.die1 + " Die2: " + this.die2;
+    }
+
+    draw(x, y)  {
+
+        let DICE_W = 100;
+
+        // For Die1
+        push();
+        translate(-width / 2 + x, -height / 2 + y);
+        fill(255);
+        box(DICE_W, DICE_W);
+
+        // Text
+        fill(0);
+        translate(0, 0, DICE_W - 40);
+        text(this.die1, 0, 0);
+
+        pop();
+
+        // For Die2
+        push();
+        let offset = DICE_W + 10;
+        translate(-width / 2 + x + offset, -height / 2 + y);
+        fill(255);
+        box(DICE_W, DICE_W);
+
+        // Text
+        fill(0);
+        translate(0, 0, DICE_W - 40);
+        text(this.die2, 0, 0);
+
+        pop();
+
+    }
+
+}
+
+class Tile  {
+    constructor(x, y, w, h)   {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.d = 30;
+        this.energyAdj = "?";
+    }
+
+    draw(playersArray, levelID)  {
+
+        // Detect if current level has player to determine the ALPHA
+        let levelHasPlayer = false;
+        for (let temp of playersArray)  {
+            if (levelID == temp.level)  {
+                levelHasPlayer = true;
+            }
+        }        
+
+        push();
+        translate(this.x, this.y);
+
+        // Draw tile box
+        if (!levelHasPlayer)    {
+            fill('rgba(0, 0, 0, 0.1)');
+            stroke('rgba(0, 0, 0, 0.1)');
+        }
+        box(this.w, this.h, this.d);
+
+        // Energy text
+        if (!levelHasPlayer)    {
+            fill('rgba(0, 0, 0, 0.5)');
+            stroke('rgba(0, 0, 0, 0.5)');
+        } else  {
+            fill(0, 0, 0);
+        }        
+        translate(0, 0, this.d / 2 + 10);
+        text(this.energyAdj, 0, 0);
+
+        // Draw player
+        for (let temp of playersArray)  {
+            if (this.x == temp.x && this.y == temp.y && levelID == temp.level)  {
+                temp.draw();
+            }
+        }
+
+        pop();
+    }
+}
+
+class Level {
+    constructor(size, id)   {
+        this.size = size;
+        this.data = [];
+        this.levelID = id;
+        this.tileSize = 150;
+    }
+
+    generate()  {
+
+        this.data = [];
+
+        for (let i = 0; i < this.size; i++) {
+            this.data[i] = []
+            for (let j = 0; j < this.size; j++) {
+                let temp = new Tile(i * this.tileSize, j * this.tileSize, this.tileSize, this.tileSize);
+                this.data[i][j] = temp;
+            }
+        }
+    }
+
+    draw(playerArray)  {
+
+        if (this.data.length == 0)  {
+            this.generate();
+        }
+
+        for (let i = 0; i < this.data.length; i++) {
+            for (let j = 0; j < this.data[i].length; j++) {
+                this.data[i][j].draw(playerArray, this.levelID);
+            }
+        }
+    }
+}
+
+class Board {
+    constructor(level, size)    {
+        this.level = level;
+        this.size = size;
+        this.data = [];
+    }
+
+    generateBoard() {
+
+        this.data = [];
+
+        for (let i = 0; i < this.level; i++)    {
+            this.data.push(new Level(this.size, i));
+            this.data[i].generate();
+            
+            // Determine energy
+            for (let j = 0; j < this.size; j++)   {
+                for (let k = 0; k < this.size; k++)   {
+                    let sum = i + j + k;
+
+                    if (sum % 3 == 0 && sum != 0)   {
+                        this.data[i].data[j][k].energyAdj = -3;                        
+                    } else if (sum % 5 == 0 && sum != 0)    {
+                        this.data[i].data[j][k].energyAdj = -2;
+                    } else if (sum % 7 == 0 && sum != 0)    {
+                        this.data[i].data[j][k].energyAdj = 2;
+                    } else  {
+                        this.data[i].data[j][k].energyAdj = 0;
+                    }
+
+                } 
+            }
+        }
+    }
+
+    draw(playerArray)  {
+
+        if (this.data.length == 0)  {
+            this.generateBoard();
+        }
+
+        for (let i = 0; i < this.data.length; i++)  {
+            push();
+            let offset = 200
+            translate(i * 100 - offset, i * 100 - offset, i * - offset / 1.5);
+            this.data[i].draw(playerArray);
+            pop();
+         
+        }
+
+    }
+}
+
+class Player    {
+    constructor(name, color)   {
+        this.name = name;
+        this.level = 0;
+        this.x = 0;
+        this.y = 0;
+        this.color = color || {r: 150, g: 50, b: 50};
+        this.energy = 10;
+    }
+
+    draw()  {
+        push();
+        fill(this.color.r, this.color.g, this.color.b);
+        rotateX(Math.PI / 2);
+        cylinder(30, 50);
+        pop();
+    }
+
+    copy(original)  {
+        let tempCopy = new Player(original.name);
+        tempCopy = original.x;
+        tempCopy = original.y;
+        tempCopy = original.level;
+        tempCopy = original.energy;
+    }
+}
