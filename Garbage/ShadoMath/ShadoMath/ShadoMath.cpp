faf434f05b6feb084bdcede7c884f936fc42aeb2
Shadi Jiha <shadijiha@gmail.com>
2019-11-18 07:12:00 -0500

    xds

--- Garbage/ShadoMath/ShadoMath/ShadoMath.cpp
+++ Garbage/ShadoMath/ShadoMath/ShadoMath.cpp
@@ -38,25 +38,12 @@ public:
 
 };
 
-int maxof(int n_args, ...)
-{
-	va_list ap;
-	va_start(ap, n_args);
-	int max = va_arg(ap, int);
-	for (int i = 2; i <= n_args; i++) {
-		int a = va_arg(ap, int);
-		if (a > max) max = a;
-	}
-	va_end(ap);
-	return max;
-}
-
 int main()
 {
 	typedef std::string string;
 	// TODO ---> Fraction operators > and < not working
 
-	std::cout << maxof(10, 2, 5, 9, 8, 7, 6, 3) << "\n\n\n" << std::endl;
+	std::cout << "\n\n\n" << std::endl;
 
 	return 0;
 }
