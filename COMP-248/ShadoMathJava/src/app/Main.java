5b1d42a7f764bf978396b4a8856980e6d2cd6cff
jshad0 <jshad0@users.noreply.github.com>
2019-11-21 11:10:41 -0500

    Update Main.java

--- COMP-248/ShadoMathJava/src/app/Main.java
+++ COMP-248/ShadoMathJava/src/app/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		ShadoMath shado = new ShadoMath();
 		final int x = 20;
 		ShadoMath.Complex z1 = shado.new Complex(2, 3);
@@ -18,5 +18,3 @@ public class Main {
 	}
 
 }
-
-
