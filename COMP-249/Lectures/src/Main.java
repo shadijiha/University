9b4c968579786167b7eb49787dc77511ff85c1a0
Shadi Jiha <shadijiha@gmail.com>
2020-01-20 17:50:04 -0500

    :/

--- COMP-249/Lectures/src/Main.java
+++ COMP-249/Lectures/src/Main.java
@@ -11,6 +11,19 @@ public abstract class Main {
 			myAccount[i] = new Account("Tom", 123 + i, 100 + (i * 20));
 			System.out.println("Balance of " + i + ": " + myAccount[i].getBalance());
 		}
+		
+		
+		int[] test = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+		
+		
+		for (int i = 0; i < test.length / 2; i++)	{
+			
+			int temp = test[i];
+			test[i] = test[test.length - i - 1];
+			test[test.length - i - 1] = temp;
+			
+		}
+		
 
 		System.out.println("Accounts:" + Account.getTotalAccounts());
 	}
