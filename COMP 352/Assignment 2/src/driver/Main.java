a7e8491329f6707c9e3d1b40713d809cd0a85577
Shadi Jiha <shadijiha@gmail.com>
2020-05-29 16:53:35 -0400

    ;

--- COMP 352/Assignment 2/src/driver/Main.java
+++ COMP 352/Assignment 2/src/driver/Main.java
@@ -1,7 +1,5 @@
 package driver;
 
-import java.util.Arrays;
-
 public class Main {
 
 	public static void main(String[] args) {
@@ -12,33 +10,14 @@ public class Main {
 		//var result = new Calculator().calculate("3 * ( 2 + 1 )");
 		//System.out.println(result);
 
-		var t = remove_duplicates(22, 61, -10, 61, 10, 9, 9, 21, 35, 22, -10, 19, 5, 77, 5, 92, 85, 21, 35, 12, 9, 61);
+		boolean a = false;
+		boolean b = false;
+		boolean c = false;
 
-		System.out.println(Arrays.toString(t));
+		System.out.println((a && b) || c);
+		System.out.println();
+
+
 	}
 
-	public static int[] remove_duplicates(int... input) {
-
-		ShadoStack<Integer> stack = new ShadoStack<>();
-
-		for (int e : input)
-			add_to_stack(stack, e);
-
-		// Turn the stack back to array
-		int[] result = new int[stack.size()];
-
-		for (int i = stack.size() - 1; i >= 0; i--)
-			result[i] = stack.pop();
-
-		return result;
-	}
-
-	public static <T extends Number & Comparable> void add_to_stack(ShadoStack<T> stack, T e) {
-
-		for (T temp : stack)
-			if (temp.equals(e))
-				return;
-
-		stack.push(e);
-	}
 }
