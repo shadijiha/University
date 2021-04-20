86ab289390d7f9214d2893b5523c0644d662cb76
Shadi Jiha <shadijiha@gmail.com>
2021-04-20 17:34:47 -0400

    Did lots of stuff

--- COMP 352/COMP 352 Assignment 1/JDocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"driver","l":"Main"},{"p":"driver","l":"RecusiveMethodFinished"},{"p":"driver","l":"Main.StatCollector"},{"p":"driver","l":"uint_t"},{"p":"driver","l":"uint32_t"}]
--- SOEN 287/src/php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*For My LocalPC*/
-// Create connection
-$conn = new mysqli("localhost", "root", "", "soen_287_project");
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-} 
-?>
