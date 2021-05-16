294a19074dd78f74de2c9e8407f09cd1ab81bc6d
Shadi Jiha <shadijiha@gmail.com>
2021-05-16 13:57:38 -0400

    hehexd

--- COMP-352/Ass 1/src/com/ProgrammingQuestion.java
+++ COMP-352/Ass 1/src/com/ProgrammingQuestion.java
@@ -3,8 +3,8 @@
  */
 package com;
 
-import java.text.*;
-import java.util.*;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class ProgrammingQuestion {
 
@@ -13,20 +13,64 @@ public class ProgrammingQuestion {
 
 
 	public static void main(String[] args) {
+		System.out.println("=========== Testing rearrangeParticipants(args...) ===========");
 
+		System.out.println("******** Before ********");
+		for (int i = 0; i < names.length; i++)
+			System.out.printf("%s, %d\n", names[i], age(pDOB[i]));
+
+		int numberOfSeniors = rearrangeParticipants(names, pDOB, names.length);
+
+		System.out.println("******** After ********");
+		for (int i = 0; i < names.length; i++)
+			System.out.printf("%s, %d\n", names[i], age(pDOB[i]));
+
+		System.out.println("There are " + numberOfSeniors + " seniors\n");
+
+		System.out.println("=========== Testing displaySeniorsIncreasingOrder(args...) ===========");
+		displaySeniorsIncreasingOrder(names, pDOB, numberOfSeniors);
 
 	}
 
 	public static int rearrangeParticipants(String[] names, String[] pDOB, int n) {
-		return rearrangeParticipants(names, pDOB, n, 0);
+		return rearrangeParticipants(names, pDOB, n, 0, 0);
 	}
 
-	public static int rearrangeParticipants(String[] names, String[] pDOB, int n, int currentIndex) {
+	public static int rearrangeParticipants(String[] names, String[] pDOB, int n, int currentIndex, int numberOfSeniors) {
 
-		return 0;
+		if (currentIndex == n)
+			return numberOfSeniors;
+
+		if (age(pDOB[currentIndex]) >= 65) {
+			swap(names, currentIndex, numberOfSeniors);
+			swap(pDOB, currentIndex, numberOfSeniors);
+			numberOfSeniors++;
+		}
+
+		currentIndex++;
+
+		return rearrangeParticipants(names, pDOB, n, currentIndex, numberOfSeniors);
 	}
 
 	public static void displaySeniorsIncreasingOrder(String[] pName, String[] pDOB, int nSenior) {
+		displaySeniorsIncreasingOrder(pName, pDOB, nSenior, 0, 0);
+	}
+
+	public static void displaySeniorsIncreasingOrder(String[] pName, String[] pDOB, int nSenior, int toDisplay, int currentIndex) {
+		for (int i = 0; i < 4; i++) {
+			if (currentIndex == nSenior || toDisplay == nSenior) {
+				System.out.printf("%s, %s using R\n", pName[toDisplay], pDOB[toDisplay]);
+				return;
+			} else {
+				if (age(pDOB[currentIndex]) < age(pDOB[toDisplay]))
+					toDisplay = currentIndex;
+
+				currentIndex++;
+				displaySeniorsIncreasingOrder(pName, pDOB, nSenior, i, currentIndex);
+			}
+		}
+
+
 	}
 
 	public static void displayNonSeniorsInreasingOrder(String[] pName, String[] pDOB, int nNoneSenior, int total) {
@@ -44,15 +88,15 @@ public class ProgrammingQuestion {
 	}
 
 	public static int age(String dateOfBirth) {
-		SimpleDateFormat formatter = new SimpleDateFormat("dd-mm-yyyy");
+		String tokens[] = dateOfBirth.split("-");
+		//DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-mm-yyyy");
 		try {
-			Date dob = formatter.parse(dateOfBirth);
-
-			//return new Period(dob.getTime(), new Date().getTime()).getYears();
-		} catch (ParseException e) {
+			LocalDate dob = LocalDate.of(Integer.parseInt(tokens[2]), Integer.parseInt(tokens[1]), Integer.parseInt(tokens[0]));
+			return Period.between(dob, LocalDate.now()).getYears();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		return 0;
+		return -1;
 	}
 }
